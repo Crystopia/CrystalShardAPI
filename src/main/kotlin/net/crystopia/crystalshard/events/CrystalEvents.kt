d81b7984ae42b972494c7c72b5e4fc1645b161fe
Jesper <jesper.l.richert@gmail.com>
2025-09-23 12:22:05 +0200

    Start with the SmartEvent System (not repleace twilights Event Methods)

---
+++ src/main/kotlin/net/crystopia/crystalshard/events/CrystalEvents.kt
@@ -0,0 +1,35 @@
+﻿package net.crystopia.crystalshard.events
+
+import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType
+
+object CrystalEvents : Listener {
+
+    val key: NamespacedKey
+        get() = NamespacedKey("crystalshard", "UUID")
+
+    var interactEvent = mutableMapOf<String, PlayerInteractEvent.() -> Unit>()
+
+    @EventHandler
+    fun onInteractEvent(event: PlayerInteractEvent) {
+        if (interactEvent.isEmpty()) {
+            return
+        }
+        if (event.item != null && event.item!!.itemMeta.persistentDataContainer.has(key)) {
+            val id = event.item!!.itemMeta.persistentDataContainer.get(
+                key,
+                PersistentDataType.STRING
+            )
+            if (event.item != null) {
+                interactEvent[id]!!.invoke(event)
+                return
+            }
+            return
+        }
+        return
+    }
+
+}
---
+++ src/main/kotlin/net/crystopia/crystalshard/extension/Event.kt
@@ -0,0 +1,17 @@
+﻿package net.crystopia.crystalshard.extension
+
+import net.crystopia.crystalshard.events.CrystalEvents
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
+
+fun ItemStack.interactEvent(action: PlayerInteractEvent.() -> Unit = {}) {
+    val id = itemMeta.persistentDataContainer.get(
+        CrystalEvents.key, PersistentDataType.STRING
+    )
+    if (id.isNullOrEmpty()) {
+        throw Exception("Item has no event interact Key!")
+    } else {
+        CrystalEvents.interactEvent[id] = action
+    }
+}
