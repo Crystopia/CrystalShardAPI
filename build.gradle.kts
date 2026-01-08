74c0b1f56e48b9c1a1948e711b76dcc12082c0c4
Jesper <jesper.l.richert@gmail.com>
2026-01-08 23:55:58 +0100

    bump version. Fixes build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("xyz.jpenilla.run-velocity") version "3.+" apply false 
+    id('xyz.jpenilla.run-paper') version: '3.+' apply false 
     id("com.gradleup.shadow") version "9.2.2" apply false
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" apply false
     kotlin("kapt") version "2.3.0" apply false
@@ -11,7 +12,7 @@ plugins {
 
 allprojects {
     group = "net.crystopia"
-    version = "0.2.21"
+    version = "0.2.22"
 
     repositories {
         mavenCentral()
