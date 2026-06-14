e37c20d30ca9f769d38dc786f24ff964c131fa5c
jespersen <jesper.l.richert@gmail.com>
2026-06-14 17:08:43 +0200

    updated to java 25 in dhl:1.21.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ subprojects {
 
 allprojects {
     group = "net.crystopia.crystalshard"
-    version = "1.2.7-SNAPSHOT-18"
+    version = "1.2.7-SNAPSHOT-19"
 
     repositories {
         mavenCentral()
