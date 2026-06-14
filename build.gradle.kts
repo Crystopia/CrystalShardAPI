c3b70c792a2480fd6d426f32e1edbed5230dd7a3
jespersen <jesper.l.richert@gmail.com>
2026-06-14 13:18:56 +0200

    fixed test java version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ subprojects {
 
 allprojects {
     group = "net.crystopia.crystalshard"
-    version = "1.2.7-SNAPSHOT-5"
+    version = "1.2.7-SNAPSHOT-6"
 
     repositories {
         mavenCentral()
