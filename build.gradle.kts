8926b3c8237ac090f65a68a08553ecd86b7a9b2d
jespersen <jesper.l.richert@gmail.com>
2026-06-14 13:23:03 +0200

    fixed paper gh action.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ subprojects {
 
 allprojects {
     group = "net.crystopia.crystalshard"
-    version = "1.2.7-SNAPSHOT-6"
+    version = "1.2.7-SNAPSHOT-7"
 
     repositories {
         mavenCentral()
